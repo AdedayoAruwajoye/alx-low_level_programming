e5891647bab829f80193bb279d1e8f2eb5071773
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-03 11:29:55 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float f;		
+	float f;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(a));
 	printf("Size of a int: %zu byte(s)\n", sizeof(b));
