df905f9aab02b856208953f2bd9ac48e61347b60
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-04 15:48:37 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point - This is a cide that prints the alphabet in lower case.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) {
+	char ch;
+
+	for (ch = "a"; ch <= "z"; ch =++)
+	{
+		putchar (ch);
+	}
+	putchar("\");
+	return (0);
+}
+
