11cd0f2a7fc33329b4c246bd1899c1449681b01d
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-04 13:23:38 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This code returns a random number and tells about its sign
+ *
+ * Return Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0) {
+	printf("The number is positive\n");
+	}
+	else if (n == 0) {
+	printf("The number is zero\n");
+	}
+	else {
+	printf("The number is negative\n");
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is my test of the variable usage in C programming.
