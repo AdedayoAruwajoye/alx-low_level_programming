9b8a6329ef2d0239d49c831083ece70b7361ef06
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-08 09:30:19 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - This program multiplies two integers
+ * nul - This program multiplies two integers
  * @a: First integer
  * @b: Second integer
- * 
  * Return: 0 (Always)
  */
 int nul(int a, int b)
