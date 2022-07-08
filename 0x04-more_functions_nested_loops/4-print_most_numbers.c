23c27ca8168914f4261d3601e282b67de09cccd9
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-08 13:11:06 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,25 +2,27 @@
 
 /**
   * print_most_numbers - Print the numbers since 0 up to 9
-  * 
+  *
   * Description: Prints the numbers excluding 2 and 4
-  * 
+  *
   * Return: The numbers since 0 up to 9
   */
-
 void print_most_numbers(void)
 {
-	int a = 0;
-	for (; a <= 9; a++)
+	int x = 0;
+
+	for (; x <= 9; x++)
 	{
-		if (a == 2 || a == 4)
+		if (x == 2 || x == 4)
 		{
 			continue;
 		}
 		else
 		{
-			_putchar(a + '0')
+			_putchar(x + '0');
 		}
 	}
+
 	_putchar('\n');
 }
+
