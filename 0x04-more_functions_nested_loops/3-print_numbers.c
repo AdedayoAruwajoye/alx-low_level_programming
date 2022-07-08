edb37b9fce966114381d05a50e3064d038870c1a
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-08 12:18:42 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,15 @@
 /**
  * print_numbers - This program prints the numbers 0 - 9
  * 
- * Return: The numbers from 0 to 9
  */
+
 void print_numbers(void)
 {
 	int a;
 
 	for (a = 48; a <= 57; a++)
 	{
-		_putchar(a + '0');
+		_putchar(a);
 	}
 	_putchar('\n');
 }
