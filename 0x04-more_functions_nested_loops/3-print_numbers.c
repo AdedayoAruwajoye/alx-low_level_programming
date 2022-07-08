d49b762cf102639f7e7c1cc317f4721da68b2f63
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-08 10:17:24 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - This program prints the numbers 0 - 9
+ * @a: This is the integer to be utilized
+ * Return: 0 (Always)
+ */
+
+int print_numbers(int a)
+{
+	while (a >= 48 && a <= 57)
+		_putchar(a);
+		a++
+}
