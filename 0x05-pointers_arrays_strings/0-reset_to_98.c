ffaa7ed8c91cac91fd07c7fc6af2f3a5ab223c70
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-12 09:04:45 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,16 +4,11 @@
 /**
  * reset_to_98 - This code takes a pointer and updates its value
  * @n: This is the first integer
- * @*p: This is my dereferencing
  *
  * Return: Nothing
  */
 
 void reset_to_98(int *n)
 {
-	int *p;
-
-	n = 40;
-	p = &n;
-	*p = 98;
+	*n = 98;
 }
