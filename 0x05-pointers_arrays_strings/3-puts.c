0265320e8caa56986a250230c03c3a96ba08ed89
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-12 11:48:07 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts - This is a function that prints a string followed by a new line
+ *@str: This is the variable that helps me count the string
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
