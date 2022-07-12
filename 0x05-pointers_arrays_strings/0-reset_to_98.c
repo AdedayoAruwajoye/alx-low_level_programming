9b361fe38e22505b9d099bc0773d35c3531dcfff
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-12 08:32:46 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Always)
+ */
+
+int n;
+int *p;
+
+int main(*p)
+{
+	n = 40;
+	*p = &n;
+	return (0);
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is my readme file for practicing C pointers, arrays and strings.
