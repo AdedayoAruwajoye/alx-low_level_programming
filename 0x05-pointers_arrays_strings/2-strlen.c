ad37b25479e65148f976997e0739404bc7a90cd4
AdedayoAruwajoye <adedayoaruwajoye@gmail.com>
2022-07-12 11:30:28 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strlen: This is a function that returns a string length
+ *@s: This is the veriable that helps me count the string
+ * Return: Nothing
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+	count++;
+	}
+	return (count);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,13 @@
 int _putchar(char c);
 void swap_int(int *a, int *b);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+void puts2(char *str);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
